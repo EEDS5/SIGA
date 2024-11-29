bb70e04df34ecee2aefe00e39788d7199f30f835
EEDS5 <duransandoval777@gmail.com>
2024-11-28 21:13:44 -0400

    feat: agregar clase z-40 para el control del z-index y mejorar el modal de proveedores con fondo oscuro

--- Backend/tailwind.config.js
+++ Backend/tailwind.config.js
@@ -11,7 +11,9 @@ module.exports = {
         background: '#FFFFFF', // Blanco para fondos principales
         section: '#F5F5F5', // Gris claro tenue para secciones y tarjetas
         primary: '#005A9C', // Azul equilibrado para énfasis
+        'primary-dark': '#00457A', // Un tono más oscuro de tu color primario para efectos hover
         secondary: '#1A1A1A', // Gris oscuro neutro para textos principales
+        labelText: '#4A4A4A', // Gris neutro para etiquetas y textos secundarios
         accent: '#CFCFCF', // Gris acentuado para bordes y elementos secundarios
         success: '#38A169', // Verde para mensajes de éxito
         warning: '#ECC94B', // Amarillo para advertencias
